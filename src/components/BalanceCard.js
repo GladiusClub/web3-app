066e54044f3561939507495885dac81538aeeeab
David Crompton <dcromp@live.co.uk>
2023-04-06 18:31:48 +0200

    button at bottom of card

--- src/components/BalanceCard.js
+++ src/components/BalanceCard.js
@@ -4,21 +4,31 @@ import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 
 export default function Balance({ amount, token }) {
   return (
     <Card sx={{ minWidth: 275, maxWidth: "30%", margin: "10px" }}>
-      <CardContent>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-          Your Balance{" "}
-        </Typography>
-        <Typography variant="h5" component="div">
-          {amount ? `${amount} ${token}` : "0.542 GLD"}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">View Transactions</Button>
-      </CardActions>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
+          height: "100%",
+        }}
+      >
+        <CardContent>
+          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+            Your Balance{" "}
+          </Typography>
+          <Typography variant="h5" component="div">
+            {amount ? `${amount} ${token}` : "0.542 GLD"}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">View Transactions</Button>
+        </CardActions>
+      </Box>
     </Card>
   );
 }
