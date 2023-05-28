246ea56f087a16bb94269311aa0e8c4788e6030e
David Crompton <dcromp@live.co.uk>
2023-05-28 21:27:00 +0200

    styling class table

--- src/components/clubSection/GroupManagment.js
+++ src/components/clubSection/GroupManagment.js
@@ -9,43 +9,92 @@ import {
   TableRow,
   Paper,
   Button,
+  tableCellClasses,
+  Typography,
 } from "@mui/material";
+import { styled } from "@mui/system";
+import ModeEditIcon from "@mui/icons-material/ModeEdit";
 
 const classes = [
-  "Adult Soccer",
-  "Youth Basketball",
-  "Senior Swimming",
-  "Women's Volleyball",
-  "Men's Tennis",
+  { name: "Adult Soccer", size: 12 },
+  { name: "Youth Basketball", size: 18 },
+  { name: "Senior Swimming", size: 8 },
+  { name: "Women's Volleyball", size: 15 },
+  { name: "Men's Tennis", size: 10 },
 ];
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    color: "white",
+    fontWeight: "bold",
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
 
 export default function ClassTable() {
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Class</TableCell>
-            <TableCell align="right">Students</TableCell>
-            <TableCell align="right">Events</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {classes.map((item, index) => (
-            <TableRow key={index}>
-              <TableCell component="th" scope="row">
-                {item}
-              </TableCell>
-              <TableCell align="right">
-                <Button variant="outlined">View Members</Button>
-              </TableCell>
-              <TableCell align="right">
-                <Button variant="outlined">View Events</Button>
-              </TableCell>
+    <div>
+      <Button color="secondary" variant="contained" sx={{ marginBottom: 2 }}>
+        Add Class +
+      </Button>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead
+            sx={{
+              background: "linear-gradient(45deg, #673ab7 30%, #3f51b5 90%)",
+            }}
+          >
+            <TableRow>
+              <StyledTableCell>Class</StyledTableCell>
+              <StyledTableCell align="right">Students</StyledTableCell>
+              <StyledTableCell align="right">Class Size</StyledTableCell>
+              <StyledTableCell align="right">Events</StyledTableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {classes.map((item, index) => (
+              <TableRow
+                key={index}
+                sx={{ "&:hover": { backgroundColor: "#f5f5f5" } }}
+              >
+                <TableCell component="th" scope="row">
+                  {item.name}
+                </TableCell>
+                <TableCell align="right">
+                  <Button
+                    startIcon={<ModeEditIcon />}
+                    color="secondary"
+                    variant="outlined"
+                  >
+                    Members
+                  </Button>
+                </TableCell>
+                <TableCell align="right">
+                  <Typography
+                    variant="h6"
+                    component="div"
+                    color="secondary"
+                    sx={{ fontWeight: "bold" }}
+                  >
+                    {item.size}
+                  </Typography>
+                </TableCell>
+                <TableCell align="right">
+                  <Button
+                    startIcon={<ModeEditIcon />}
+                    color="secondary"
+                    variant="outlined"
+                  >
+                    Events
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
   );
 }
