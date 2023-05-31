a864aef024a3f075d14b3627d1e108ba550d7d46
David Crompton <dcromp@live.co.uk>
2023-05-31 18:03:26 +0200

    added a stepper

--- src/components/clubSection/GroupManagment.js
+++ src/components/clubSection/GroupManagment.js
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
+import Stepper from "@mui/material/Stepper";
+import Step from "@mui/material/Step";
+import StepLabel from "@mui/material/StepLabel";
 
 import {
   Table,
@@ -11,6 +14,9 @@ import {
   Button,
   tableCellClasses,
   Typography,
+  Dialog,
+  DialogTitle,
+  Box,
 } from "@mui/material";
 import { styled } from "@mui/system";
 import ModeEditIcon from "@mui/icons-material/ModeEdit";
@@ -34,9 +40,24 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
 }));
 
 export default function ClassTable() {
+  const [openDialog, setOpenDialog] = useState(false); // State for dialog visibility
+
+  const handleOpenDialog = () => {
+    setOpenDialog(true);
+  };
+
+  const handleCloseDialog = () => {
+    setOpenDialog(false);
+  };
+
   return (
     <div>
-      <Button color="secondary" variant="contained" sx={{ marginBottom: 2 }}>
+      <Button
+        color="secondary"
+        variant="contained"
+        sx={{ marginBottom: 2 }}
+        onClick={handleOpenDialog}
+      >
         Add Class +
       </Button>
       <TableContainer component={Paper}>
@@ -95,6 +116,122 @@ export default function ClassTable() {
           </TableBody>
         </Table>
       </TableContainer>
+      <AddClassDialog open={openDialog} handleClose={handleCloseDialog} />
     </div>
+  );
+}
+
+function AddClassDialog({ open, handleClose }) {
+  const handleSubmit = () => {
+    // Close the dialog
+    handleClose();
+  };
+
+  return (
+    <Dialog open={open} onClose={handleClose}>
+      <DialogTitle>Add Class</DialogTitle>
+      <Box sx={{ margin: "16px" }}>
+        <HorizontalLinearStepper handleSubmit={handleSubmit} />
+      </Box>
+    </Dialog>
+  );
+}
+
+const steps = [
+  "Pick Class Name",
+  "Add Members to Class",
+  "Add Events to Class",
+];
+
+function HorizontalLinearStepper({ handleSubmit }) {
+  const [activeStep, setActiveStep] = useState(0);
+  const [className, setClassName] = useState("");
+  const [members, setMembers] = useState([]);
+  const [events, setEvents] = useState([]);
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+
+    if (activeStep === steps.length - 1) {
+      handleSubmit();
+    }
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleClassNameChange = (event) => {
+    setClassName(event.target.value);
+  };
+
+  const handleMembersChange = (event) => {
+    setMembers(event.target.value);
+  };
+
+  const handleEventsChange = (event) => {
+    setEvents(event.target.value);
+  };
+
+  const renderStepContent = () => {
+    switch (activeStep) {
+      case 0:
+        return (
+          <div>
+            <Typography>Class Name:</Typography>
+            <input
+              type="text"
+              value={className}
+              onChange={handleClassNameChange}
+            />
+          </div>
+        );
+      case 1:
+        return (
+          <div>
+            <Typography>Members:</Typography>
+            <input type="text" value={members} onChange={handleMembersChange} />
+          </div>
+        );
+      case 2:
+        return (
+          <div>
+            <Typography>Events:</Typography>
+            <input type="text" value={events} onChange={handleEventsChange} />
+          </div>
+        );
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Stepper activeStep={activeStep}>
+        {steps.map((label, index) => (
+          <Step key={label}>
+            <StepLabel>{label}</StepLabel>
+          </Step>
+        ))}
+      </Stepper>
+
+      <React.Fragment>
+        <Box sx={{ mt: 2 }}>{renderStepContent()}</Box>
+        <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
+          <Button
+            color="inherit"
+            disabled={activeStep === 0}
+            onClick={handleBack}
+            sx={{ mr: 1 }}
+          >
+            Back
+          </Button>
+          <Box sx={{ flex: "1 1 auto" }} />
+          <Button onClick={handleNext}>
+            {activeStep === steps.length - 1 ? "Submit" : "Next"}
+          </Button>
+        </Box>
+      </React.Fragment>
+    </Box>
   );
 }
