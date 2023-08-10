561aa20ea8c56c7c568c14255fc7ec0e5dbd09fd
David Crompton <dcromp@live.co.uk>
2023-08-10 17:51:11 +0200

    transaction send loading

--- src/components/SendTransactionCard.js
+++ src/components/SendTransactionCard.js
@@ -10,12 +10,14 @@ import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import InputLabel from "@mui/material/InputLabel";
 import TextField from "@mui/material/TextField";
+import CircularProgress from "@mui/material/CircularProgress";
 import { useFirebase } from "./contexts/firebaseContext";
 
 const SendTransactionCard = ({ clubMembers }) => {
   const { auth } = useFirebase();
   const [selectedAddress, setSelectedAddress] = React.useState("");
   const [amount, setAmount] = React.useState(""); // State for amount
+  const [isLoading, setIsLoading] = React.useState(false);
 
   const handleSend = async () => {
     // Ensure an address is selected before proceeding
@@ -28,6 +30,7 @@ const SendTransactionCard = ({ clubMembers }) => {
     const idToken = await auth.currentUser.getIdToken(true);
 
     try {
+      setIsLoading(true);
       const response = await fetch(
         "https://us-central1-wallet-login-45c1c.cloudfunctions.net/mumbai_token_transfer",
         {
@@ -45,8 +48,10 @@ const SendTransactionCard = ({ clubMembers }) => {
       );
 
       const data = await response.json();
+      setIsLoading(false);
       console.log(data);
     } catch (error) {
+      setIsLoading(false);
       console.error("Error sending transaction:", error);
     }
   };
@@ -103,11 +108,23 @@ const SendTransactionCard = ({ clubMembers }) => {
             </Box>
           </Box>
         </CardContent>
-        <CardActions sx={{ justifyContent: "flex-end" }}>
-          <Button color="secondary" onClick={handleSend}>
-            Send
-          </Button>
-        </CardActions>
+        {isLoading ? (
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "flex-end",
+              paddingRight: "1rem",
+            }}
+          >
+            <CircularProgress color="secondary" />
+          </Box>
+        ) : (
+          <CardActions sx={{ justifyContent: "flex-end" }}>
+            <Button color="secondary" onClick={handleSend}>
+              Send
+            </Button>
+          </CardActions>
+        )}
       </Box>
     </Card>
   );
